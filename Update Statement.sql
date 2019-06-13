13741155cd267afe9c97cdd2db8c8c45c0bc417f
Kebinoy <kevin.rivera20@gmail.com>
2019-06-13 11:27:06 +0800

    Edit the Update Statement

--- Update Statement.sql
+++ Update Statement.sql
@@ -8,10 +8,10 @@ The UPDATE statement is used to modify the existing records in a table.
 ==== >> Sample Table "Student"
 
 student_id	| firstname		| middlename	| lastname		| gender	| bdate
-5			| Kevin John	| Ayudante		| Rivera		| Male		| 4/20/1992
-6			| Jan			| S				| Mante			| Male		| 3/21/1987
-7			| Anton 		| B				| Alonsabe		| Male		| 4/21/1973
-8			| Karen			| B				| Ybanez		| Female	| 6/20/1972
+5			| Kevin			| A				| Rivera		| Male		| 01/01/1995
+6			| Jan			| S				| Mante			| Male		| 3/21/1980
+7			| Anton 		| B				| Alonsabe		| Male		| 9/21/1920
+8			| Karen			| B				| Ybanez		| Female	| 7/20/1980
 
 */
 UPDATE [dbo].[student]
